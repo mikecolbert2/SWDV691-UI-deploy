5a7e13caa250501fc729041915f64558cccfd70f
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-03 21:17:59 -0500

    home & about pages with beginning Bootstrap 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { AboutComponent } from './components/pages/about/about.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { UserManagerComponent } from './components/pages/admin/user-manager/user-manager.component';
+
 
 const routes: Routes = [
-  { path: '', component: TasksComponent }
-  //{ path: 'about', component: AboutComponent }
+  { path: '', component: WelcomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'admin/user-manager', component: UserManagerComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,28 @@ import { AppComponent } from './app.component';
 
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { UserManagerComponent } from './components/pages/admin/user-manager/user-manager.component';
+import { LoginComponent } from './components/pages/auth/login/login.component';
+import { CreateAccountComponent } from './components/pages/auth/create-account/create-account.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    TaskListComponent
+    TaskListComponent,
+    WelcomeComponent,
+    AboutComponent,
+    UserManagerComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
